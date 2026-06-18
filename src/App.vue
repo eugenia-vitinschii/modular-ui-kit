0c7333a7d82d8bfdfd93e672ecb50aa279435051
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-06-18 19:01:03 +0300

    chore: clean up default project template and setup styles structure

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">
+    <header>
+      <h1>Hi!</h1>
+    </header>
+    <div class="content">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error aspernatur dignissimos tempore adipisci
+        doloribus aut fuga est rerum, optio ipsam! Ipsam aut accusantium minus tenetur. Laborum tempore distinctio enim
+        numquam.</p>
+    </div>
+    <div class="header">
+      <p>>modular-ui-kit</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
