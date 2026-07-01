4f833f4f11ceec747cce09c3de0c8cde6f566214
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-07-01 18:13:27 +0300

    feat(base-select): add BaseSelect preview.Closes #4

--- src/views/FormElementsView.vue
+++ src/views/FormElementsView.vue
@@ -7,6 +7,7 @@
             </div>
             <div class="page__content">
                <div class="form section">
+                  <p class="subheading">Input</p>
                   <div class="form__item">
                      <base-input v-model="textValue" label="*Name" placeholder="Enter your username..." />
                      <!-- <p class="body-text" v-if="textValue">Current value: <strong>{{ textValue }}</strong> </p> -->
@@ -26,6 +27,21 @@
                      <base-input v-model="dateValue" type="date" label="Your birthday" />
                   </div>
                </div>
+               <div class="form section">
+                  <p class="subheading">Select</p>
+                  <div class="form__item">
+                     <base-select v-model="selectedCountry" :options="countries" placeholder="Select country..."
+                        label="Your Country" />
+                  </div>
+                  <div class="form__item">
+                     <base-select v-model="selectedCity" :options="cities" placeholder="Select sity..."
+                        label="Your City" error="Please select your city to continue" />
+                  </div>
+                  <div class="form__item">
+                     <base-select v-model="lockedWorkModel" :options="workModelOptions" placeholder="Select sity..."
+                        label="Preferred Work Model" hint="Work model cannot be changed for this role." disabled />
+                  </div>
+               </div>
             </div>
          </div>
       </div>
@@ -38,6 +54,8 @@ import { ref, computed } from 'vue'
 
 /* COMPONENTS */
 import BaseInput from '@/components/ui/form/BaseInput.vue';
+import BaseSelect from '@/components/ui/form/BaseSelect.vue';
+
 
 /* variables for v-model */
 const textValue = ref('')
@@ -50,5 +68,28 @@ const emailError = computed(() => {
    if (!emailValue.value) return ''
    return emailValue.value.includes('@') ? '' : 'Invalid email'
 })
+
+//select
+const selectedCountry = ref('')
+const countries = [
+   { value: 'usa', label: 'United States' },
+   { value: 'uk', label: 'United Kingdom' },
+   { value: 'germany', label: 'Germany' },
+   { value: 'japan', label: 'Japan' },
+   { value: 'canada', label: 'Canada' },
+]
+
+const selectedCity = ref('')
+const cities = [
+   { value: 'kyiv', label: 'Kyiv' },
+   { value: 'lviv', label: 'Lviv' }
+]
+
+const lockedWorkModel = ref('remote')
+const workModelOptions = [
+   { value: 'office', label: 'In-office' },
+   { value: 'hybrid', label: 'Hybrid' },
+   { value: 'remote', label: 'Fully Remote' }
+]
 
 </script>
