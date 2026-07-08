95363f6ca3668dfc335d1f51a5939b9cc0de97f8
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-07-08 17:03:40 +0300

    feat(error-state): creat reusable empty state component. Closes #13

---
+++ src/components/ui/feedback/ErrorState.vue
@@ -0,0 +1,26 @@
+<template>
+   <div class="error-state">
+      <div class="error-state__text">
+         <h3 class="subheading">{{ title }}</h3>
+         <p class="body-text">{{ description }}</p>
+      </div>
+      <div class="error-state__action">
+         <slot name="error-action"></slot>
+      </div>
+   </div>
+</template>
+
+<script setup lang="ts">
+
+/* PROPS */
+const props = withDefaults(
+   defineProps<{
+      title?: string
+      description?: string
+   }>(),
+   {
+      title: "Oops! Something went wrong",
+      description: 'Failed to fetch data. Please try again.',
+   }
+)
+</script>
