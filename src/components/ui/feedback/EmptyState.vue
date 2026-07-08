8e6219e802d5501387ac56081c3562c0f41afab6
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-07-08 16:52:34 +0300

    feat(empty-state): create reusable EmptyState component with slot

---
+++ src/components/ui/feedback/EmptyState.vue
@@ -0,0 +1,29 @@
+<template>
+   <div class="empty-state" :class="`empty-state--${variant}`">
+      <div class="empty-state__text">
+         <h3 class="subheading">{{ title }}</h3>
+         <p class="body-text">{{ description }}</p>
+      </div>
+      <div class="empty-state__actions">
+         <slot name="action"></slot>
+      </div>
+   </div>
+</template>
+
+<script setup lang="ts">
+/* VARIANTS */
+type Variant = 'default' | 'accent'
+
+withDefaults(
+   defineProps<{
+      title?: string
+      description?: string
+      variant?: Variant
+   }>(),
+   {
+      variant: 'default',
+      title: "No data available",
+      description: 'There is nothing to display here at the moment. Please try refreshing the page',
+   }
+)
+</script>
