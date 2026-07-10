6ace3d292bf1c02238b0a342d053b5b221fb665f
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-07-10 17:06:36 +0300

    feat(base-skeleton): create base skeleton component

---
+++ src/components/ui/feedback/BaseSkeleton.vue
@@ -0,0 +1,11 @@
+<template>
+   <div class="base-skeleton">
+      <div class="base-skeleton__circle shimmer"></div>
+      <div class="base-skeleton__title shimmer"></div>
+      <div class="base-skeleton__content shimmer"></div>
+   </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
