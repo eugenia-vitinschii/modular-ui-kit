ccc39c6b400be65b3bd29397f9c0e6918940c255
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-07-13 15:49:58 +0300

    feat(base-table): create base table component

---
+++ src/components/ui/data-display/BaseTable.vue
@@ -0,0 +1,36 @@
+<template>
+   <table class="base-table">
+      <thead>
+         <tr>
+            <th v-for="c in columns" :key="c.key" class="body-text">{{ c.title }}</th>
+            <th v-if="$slots.tableActions" class="body-text">Actions</th>
+         </tr>
+      </thead>
+      <tbody>
+         <tr v-for="row in data" :key="row.id">
+            <td v-for="c in columns" :key="c.key" class="body-text">
+               {{ row[c.key] }}
+            </td>
+            <td v-if="$slots.tableActions">
+               <div class="base-table__actions">
+                  <slot name="tableActions" :row="row"></slot>
+               </div>
+            </td>
+         </tr>
+      </tbody>
+   </table>
+</template>
+
+<script setup lang="ts">
+
+export interface TableColumn {
+   key: string
+   title: string
+}
+
+defineProps<{
+   columns: TableColumn[]
+   data: any
+}>()
+
+</script>
