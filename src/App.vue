d71bb32d0e9f8cd3ada2393debaa8990dda348d5
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-07-17 15:24:56 +0300

    feat(base-toast): create demo view for toast.Closes #14

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <div class="content">
       <base-breadcrumbs />
       <router-view />
+      <base-toast-container />
     </div>
     <app-footer />
   </div>
@@ -14,5 +15,5 @@
 import AppHeader from './widgets/AppHeader.vue';
 import AppFooter from './widgets/AppFooter.vue';
 import BaseBreadcrumbs from './components/ui/navigation/BaseBreadcrumbs.vue';
-
+import BaseToastContainer from './components/ui/feedback/BaseToastContainer.vue';
 </script>
---
+++ src/views/FeedbackDemoView.vue
@@ -0,0 +1,44 @@
+<template>
+   <div class="page">
+      <div class="container">
+         <div class="page__wrapper">
+            <div class="page__header">
+               <h1 class="heading">Toast & Modals Preview</h1>
+            </div>
+            <div class="page__content">
+               <div class="demo-controls section">
+                  <h2 class="subheading">Demo Controls</h2>
+                  <base-button @click="triggerToast('success')" variant="ghost">succes toast</base-button>
+                  <base-button @click="triggerToast('error')" variant="danger">error toast</base-button>
+                  <base-button @click="triggerToast('warning')" variant="primary">warning toast</base-button>
+                  <base-button @click="triggerToast('info')" variant="secondary">info toast</base-button>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</template>
+
+<script setup lang="ts">
+/* Components */
+import BaseButton from '@/components/ui/buttons/BaseButton.vue';
+
+/* */
+import { useToast, type ToastType } from '@/components/ui/feedback/useToast';
+
+const { addToast } = useToast()
+
+// messages for demo
+const toastMessages: Record<ToastType, string> = {
+   success: 'Success! Everything went smoothly.',
+   error: 'Oops! Something went terribly wrong.',
+   warning: 'Warning! Please review your input files.',
+   info: 'Heads up! New updates are available.'
+}
+
+// trigger
+function triggerToast(type: ToastType) {
+   const message = toastMessages[type]
+   addToast(message, type, 3000)
+}
+</script>
