75592efac88e8354488a34a95ce5b9ff130b48f4
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-07-20 17:42:42 +0300

    feat(app): create types for variants, sizes and types

---
+++ src/types/ui.types.ts
@@ -0,0 +1,15 @@
+/* ==========  UI TYPES ========== */
+
+/* UI VARIANTS */
+export type UIVariant = 'primary' | 'danger' | 'warning' | 'success' | 'secondary'
+export type UISize = 'sm' | 'md' | 'lg'
+
+/* BUTTONS */
+export type UIButtonVariant = UIVariant | 'ghost'
+export type UIButtonType = "button" | "submit" | "reset"
+
+/* EMPTY STATE */
+export type UIEmptyStateVariant = 'secondary' | 'primary'
+
+/* INPUT TYPE */
+export type UIInputType = "text" | "email" | "password" | "search" | "date" | "tel" | "number"
