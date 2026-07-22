ddc7022a1b1e11977837ecd37e108706732ea35b
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-07-22 16:30:25 +0300

    feat(base-text): add new types, create component

---
+++ src/components/ui/typography/BaseText.vue
@@ -0,0 +1,27 @@
+<template>
+   <component :is="as" :class="[type, `txt--${align}`, `txt--${variant}`]">
+      <slot></slot>
+   </component>
+</template>
+
+<script setup lang="ts">
+
+/* TYOES */
+import type { UITextVariant, UITextType, UITextAlign } from '@/types/ui.types';
+
+/* PROPS */
+interface Props {
+   variant?: UITextVariant
+   as?: string
+   type?: UITextType
+   align?: UITextAlign
+}
+
+const props = withDefaults(defineProps<Props>(), {
+   variant: 'secondary',
+   align: 'left',
+   as: 'p',
+   type: 'body-text'
+})
+
+</script>
--- src/types/ui.types.ts
+++ src/types/ui.types.ts
@@ -4,6 +4,11 @@
 export type UIVariant = 'primary' | 'danger' | 'warning' | 'success' | 'secondary'
 export type UISize = 'sm' | 'md' | 'lg'
 
+/* TEXT */
+export type UITextType = 'heading' | 'subheading' | 'body-text'
+export type UITextAlign = 'left' | 'center' | 'right'
+export type UITextVariant = UIVariant | 'muted'
+
 /* BUTTONS */
 export type UIButtonVariant = UIVariant | 'ghost'
 export type UIButtonType = "button" | "submit" | "reset"
