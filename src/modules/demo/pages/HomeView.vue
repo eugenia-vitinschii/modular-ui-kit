828e83cd74fcf7acd1e8be94731bfa7236cd98f2
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-07-23 15:50:21 +0300

    refactor(demo): create demo molude

---
+++ src/modules/demo/pages/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+   <div class="page">
+      <div class="container">
+         <div class="page__wrapper">
+            <div class="page__header">
+               <base-text as="h1" :type="'heading'">Modular UI Kit</base-text>
+               <base-text as="h2" :type="'subheading'">Minimalist & Accessible Design System</base-text>
+            </div>
+            <div class="page__content">
+               <base-text as="a" href='https://github.com/eugenia-vitinschii/modular-ui-kit'>
+                  <img alt='GitHub Clones'
+                     src='https://img.shields.io/badge/dynamic/json?color=success&label=Clone&query=count&url=https://gist.githubusercontent.com/eugenia-vitinschii/2e6247835bbf3928be881ca260cad051/raw/clone.json&logo=github'>
+               </base-text>
+            </div>
+         </div>
+      </div>
+   </div>
+</template>
+
+<script setup lang="ts">
+/* COMPONENTS */
+import BaseText from '@/components/ui/typography/BaseText.vue';
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,90 +2,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import type { RouteRecordRaw } from "vue-router";
 
-
-/* ERROR ROUTES*/
+/* ==== DEMO ROUTES ==== */
+import { demoRoutes } from '@/modules/demo/routes/demo.routes';
+/* === ERROR ROUTES === */
 import { errorRoutes } from '@/modules/errors/routes/error.routes.ts';
 
-const routes: RouteRecordRaw[] = [
-   {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue'),
-      meta: { title: "Home" }
-   },
-   {
-      path: '/ui',
-      name: 'ui-kit',
-      children: [
-         {
-            path: 'text',
-            name: 'ui-text',
-            component: () => import('../views/TextView.vue'),
-            meta: { title: "Text" },
-         },
-         {
-            path: 'buttons',
-            name: 'ui-buttons',
-            component: () => import('../views/ButtonsView.vue'),
-            meta: { title: "Buttons" },
-         },
-         {
-            path: 'flow',
-            name: 'ui-flow',
-            component: () => import('../views/ComponentFlowView.vue'),
-            meta: { title: "Component Flow" },
-         },
-         {
-            path: 'table',
-            name: 'ui-table',
-            component: () => import('../views/TableView.vue'),
-            meta: { title: "Table" },
-         }, {
-            path: 'feedback',
-            name: 'ui-feedback',
-            component: () => import('../views/FeedbackDemoView.vue'),
-            meta: { title: "Toast and Popup" },
-         },
-         {
-            path: 'form',
-            name: 'ui-form',
-            meta: { title: "Form" },
-            children: [
-               {
-                  path: 'inputs',
-                  name: 'ui-form-inputs',
-                  component: () => import('../views/InputView.vue'),
-                  meta: { title: "Inputs" }
-               },
-               {
-                  path: 'selects',
-                  name: 'ui-form-selects',
-                  component: () => import('../views/SelectView.vue'),
-                  meta: { title: "Selects" }
-               },
-               {
-                  path: 'textareas',
-                  name: 'ui-form-textareas',
-                  component: () => import('../views/TextareaView.vue'),
-                  meta: { title: "Textareas" }
-               },
-               {
-                  path: 'checkboxes',
-                  name: 'ui-form-checkboxes',
-                  component: () => import('../views/CheckboxView.vue'),
-                  meta: { title: "Checkboxes" }
-               },
-               {
-                  path: 'radio',
-                  name: 'ui-form-radio',
-                  component: () => import('../views/RadioView.vue'),
-                  meta: { title: "Radio" }
-               },
-            ]
-         },
 
-      ]
-   },
+const routes: RouteRecordRaw[] = [
+   ...demoRoutes,
    ...errorRoutes,
 ]
 
