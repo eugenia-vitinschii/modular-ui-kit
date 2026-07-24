e9721817f53473d5752097cceabcf49b0e505cc1
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-07-24 18:18:01 +0300

    feat(landing): create home page. Closes #19

---
+++ src/modules/landing/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class="page">
+      <div class="container">
+         <div class="page__wrapper  error-page-wrapper">
+            <base-text :type="'heading'" as="h1">HOME PAGE</base-text>
+         </div>
+      </div>
+   </div>
+</template>
+
+<script setup lang="ts">
+/* COMPONENTS */
+import BaseText from '@/components/ui/typography/BaseText.vue';
+
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,13 +4,17 @@ import type { RouteRecordRaw } from "vue-router";
 
 /* ==== DEMO ROUTES ==== */
 import { demoRoutes } from '@/modules/demo/routes/demo.routes';
+
 /* === ERROR ROUTES === */
 import { errorRoutes } from '@/modules/errors/routes/error.routes.ts';
 
+/* === LANDING ROUTES === */
+import { landingRoutes } from '@/modules/landing/routes/landing.routes';
 
 const routes: RouteRecordRaw[] = [
    ...demoRoutes,
    ...errorRoutes,
+   ...landingRoutes,
 ]
 
 const router = createRouter({
